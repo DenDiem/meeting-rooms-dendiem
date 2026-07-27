fe96cb272756ad404c76957623796d1999d622b2
Denys Baranov <36038876+DenDiem@users.noreply.github.com>
2026-07-28 02:33:24 +0300

    Initialize
    Initialize meeting-rooms repository.

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/coverage/**'],
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  prettier,
+);
